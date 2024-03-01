4ba8739b6fa7b4009e9e4fcf16930ed2f95506d7
Matthew-Tiu <mattjtiu@gmail.com>
2024-03-01 14:39:05 +0800

    Ninja.js and Drawing Cricles Refactor

--- JavaScript/Ninja Battle/ninja.js
+++ JavaScript/Ninja Battle/ninja.js
@@ -2,7 +2,16 @@ var ninja1 = {
     hp: 100,
     strength: 15,
     attack: function () {
-        return Math.floor(Math.random() * this.strength);
+        let damage = Math.floor(Math.random() * (this.strength + 1));
+        ninja2.hp -= damage;
+        console.log(
+            "Ninja1 attacks Ninja2 and does a damage of " +
+                damage +
+                "! Ninja1 health: " +
+                this.hp +
+                ". Ninja2 health: " +
+                ninja2.hp
+        );
     },
 };
 
@@ -10,36 +19,29 @@ var ninja2 = {
     hp: 150,
     strength: 10,
     attack: function () {
-        return Math.floor(Math.random() * this.strength);
+        let damage = Math.floor(Math.random() * (this.strength + 1));
+        ninja1.hp -= damage;
+        console.log(
+            "Ninja2 attacks Ninja1 and does a damage of " +
+                damage +
+                "! Ninja1 health: " +
+                ninja1.hp +
+                ". Ninja2 health: " +
+                this.hp
+        );
     },
 };
 
 for (let i = 1; i <= 10; i++) {
-    let damage1 = ninja1.attack();
-    let damage2 = ninja2.attack();
-    ninja2.hp -= damage1;
-    ninja1.hp -= damage2;
     console.log("===Round "+i+"===")
-    console.log(
-        "Ninja1 attacks Ninja2 and does a damage of " +
-            damage1 +
-            "! Ninja1 health: " +
-            ninja1.hp +
-            ". Ninja2 health: " +
-            ninja2.hp
-    );
-    console.log(
-        "Ninja2 attacks Ninja1 and does a damage of " +
-            damage2 +
-            "! Ninja1 health: " +
-            ninja1.hp +
-            ". Ninja2 health: " +
-            ninja2.hp
-    );
+    ninja1.attack();
+    ninja2.attack();
 }
 
 if (ninja1.hp > ninja2.hp) {
     console.log("NINJA1 WINS!!!!!");
-} else {
+} else if(ninja2.hp>ninja1.hp){
     console.log("NINJA2 WINS!!!!!");
+} else {
+    console.log("DRAW!!!!!")
 }
