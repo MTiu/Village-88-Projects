b6e802d717db905a15648597fc7123405ead78d2
Matthew-Tiu <mattjtiu@gmail.com>
2024-03-06 18:29:16 +0800

    Stringlib in ES6 Init commit

---
+++ JavaScript/18 - String Module  ES6/app.js
@@ -0,0 +1,6 @@
+const string_module = require("./stringlib")();
+
+console.log(string_module.concat("Village", "88"));
+console.log(string_module.repeat("ha", 3));
+console.log(string_module.toString(5));
+console.log(string_module.charAt("nice", 2));
---
+++ JavaScript/18 - String Module  ES6/stringlib.js
@@ -0,0 +1,20 @@
+module.exports = () => {
+    return {
+        concat(word1, word2) {
+            return "" + word1 + word2;
+        },
+        repeat(word, times) {
+            let string = '';
+            for (let i = 0; i < times; i++) {
+                string += word + "";
+            }
+            return string;
+        },
+        toString(input) {
+            return input + "";
+        },
+        charAt(word, index) {
+            return word[index];
+        },
+    };
+};
