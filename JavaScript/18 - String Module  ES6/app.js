9e28c9b2ccedd9ee76b6ca36cfd9ab99028ea2f5
Matthew-Tiu <mattjtiu@gmail.com>
2024-03-06 18:57:26 +0800

    Stringlib in ES6 refactor

--- JavaScript/18 - String Module  ES6/app.js
+++ JavaScript/18 - String Module  ES6/app.js
@@ -1,6 +1,7 @@
 const string_module = require("./stringlib")();
+const my_class = new string_module();
 
-console.log(string_module.concat("Village", "88"));
-console.log(string_module.repeat("ha", 3));
-console.log(string_module.toString(5));
-console.log(string_module.charAt("nice", 2));
+console.log(my_class.concat("Village", "88"));
+console.log(my_class.repeat("ha", 3));
+console.log(my_class.toString(5));
+console.log(my_class.charAt("nice", 2));
--- JavaScript/18 - String Module  ES6/stringlib.js
+++ JavaScript/18 - String Module  ES6/stringlib.js
@@ -1,20 +1,20 @@
 module.exports = () => {
-    return {
+    return class StringLib {
         concat(word1, word2) {
             return "" + word1 + word2;
-        },
+        }
         repeat(word, times) {
-            let string = '';
+            let string = "";
             for (let i = 0; i < times; i++) {
                 string += word + "";
             }
             return string;
-        },
+        }
         toString(input) {
             return input + "";
-        },
+        }
         charAt(word, index) {
             return word[index];
-        },
+        }
     };
 };
