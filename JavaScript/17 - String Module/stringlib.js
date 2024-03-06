c1a475e7f00fe75d757e74609303133bcdce48a6
Matthew-Tiu <mattjtiu@gmail.com>
2024-03-06 18:24:24 +0800

    String Module Init Commit

---
+++ JavaScript/17 - String Module/stringlib.js
@@ -0,0 +1,20 @@
+module.exports = function () {
+    return {
+        concat: function (word1, word2) {
+            return "" + word1 + word2;
+        },
+        repeat: function (word, times) {
+            let string = '';
+            for (let i = 0; i < times; i++) {
+                string += word + "";
+            }
+            return string;
+        },
+        toString: function (input) {
+            return input + "";
+        },
+        charAt: function (word, index) {
+            return word[index];
+        },
+    };
+};
