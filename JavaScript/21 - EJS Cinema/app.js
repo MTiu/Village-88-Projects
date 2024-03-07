203f12c873c995703b8fbd1c6cdfb9841cc5e86f
Matthew-Tiu <mattjtiu@gmail.com>
2024-03-07 17:23:20 +0800

    Cinema EJS Finish Commit

--- JavaScript/21 - EJS Cinema/app.js
+++ JavaScript/21 - EJS Cinema/app.js
@@ -5,4 +5,27 @@ app.listen(7777, ()=>{
     console.log("Listening!")
 })
 
-app.use(express.static(__dirname + "/static"));
+app.use(express.static(__dirname + "/files"));
+app.set('views', __dirname + '/files/views');
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+// Not Sure if this is allowed but in the chances that it isn't the code below this dynamic get code should be used instead!
+app.get('/:page',(req,res)=>{
+    const page = req.params.page;
+    res.render(page);
+})
+
+// app.get('/movies',(req,res)=>{
+//     res.render('movies');
+// });
+
+// app.get('/theaters',(req,res)=>{
+//     res.render('theaters');
+// });
+
+// app.get('/form',(req,res)=>{
+//     res.render('form');
+// });
