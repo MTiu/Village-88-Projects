9d2eb534543e989f006f838ab8c707bf85cf6a7f
Matthew-Tiu <mattjtiu@gmail.com>
2024-03-27 15:11:16 +0800

    Certificate Ruby Init Commit

---
+++ Ruby/09 - Certificate/certificate.rb
@@ -0,0 +1,15 @@
+class Certificate
+    attr_accessor :title, :awardee
+    def initialize(title, awardee)
+        @title = title
+        @awardee = awardee
+    end
+
+    def announce
+        puts "#{@title}: #{@awardee}"
+    end
+end
+
+cert = Certificate.new("Proficient in Backend", "Philip Campani")
+puts cert.title # => "Proficient in Backend"
+cert.announce  # => "Proficient in Backend: Philip Campani"
